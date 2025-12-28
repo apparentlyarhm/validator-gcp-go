8b00a2c0b389014c9d28b5e6d66dc735618f7ef0
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-12-28 18:38:59 +0530

    refactor

--- internal/service/validator.go
+++ internal/service/validator.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	"context"
-	"log/slog"
+	"log"
 
 	compute "cloud.google.com/go/compute/apiv1"
 	"cloud.google.com/go/storage"
@@ -13,36 +13,41 @@ type ValidatorService struct {
 	cfg *config.Config
 
 	// clients
-	firewallsClient *compute.FirewallsClient
-	instancesClient *compute.InstancesClient
-	storageClient   *storage.Client
-
-	logger *slog.Logger
+	firewallsClient   *compute.FirewallsClient
+	instancesClient   *compute.InstancesClient
+	machineTypeClient *compute.MachineTypesClient
+	storageClient     *storage.Client
 }
 
-func NewValidatorService(cfg *config.Config, l *slog.Logger) (*ValidatorService, error) {
+func NewValidatorService(cfg *config.Config) (*ValidatorService, error) {
 	ctx := context.Background()
 
+	// All of this can be thought of @Autowired equivalent
 	fwClient, err := config.NewFirewallsClient(ctx)
 	if err != nil {
-		return nil, err
+		log.Fatalf("[INIT]: Could not start up Firewall Client")
 	}
 
 	instClient, err := config.NewInstancesClient(ctx)
 	if err != nil {
-		return nil, err
+		log.Fatalf("[INIT]: Could not start up Instances Client")
 	}
 
 	storageClient, err := config.NewStorageClient(ctx)
 	if err != nil {
-		return nil, err
+		log.Fatalf("[INIT]: Could not start up Storage Client")
+	}
+
+	mchTypeClient, err := config.NewMachinesTypeClient(ctx)
+	if err != nil {
+		log.Fatalf("[INIT]: Could not start up Machine Types Client")
 	}
 
 	return &ValidatorService{
-		cfg:             cfg,
-		firewallsClient: fwClient,
-		instancesClient: instClient,
-		storageClient:   storageClient,
-		logger:          l,
+		cfg:               cfg,
+		firewallsClient:   fwClient,
+		instancesClient:   instClient,
+		storageClient:     storageClient,
+		machineTypeClient: mchTypeClient,
 	}, nil
 }
