d6b6d7a26c92422707e61527f806e0376de0570a
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-12-29 18:12:04 +0530

    Add client inits + basic scaffolding for validator service + implement Get vm details api

--- internal/config/clients.go
+++ internal/config/clients.go
@@ -2,28 +2,52 @@ package config
 
 import (
 	"context"
+	"log"
 
 	compute "cloud.google.com/go/compute/apiv1"
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
 )
 
-func NewInstancesClient(ctx context.Context) (*compute.InstancesClient, error) {
-	// TODO: Initialize and return GCP InstancesClient
+func NewInstancesClient(ctx context.Context, o option.ClientOption) *compute.InstancesClient {
+	c, e := compute.NewInstancesRESTClient(ctx, o)
+	if e != nil {
+		log.Fatalf("[INIT] Could not instantiate Instances Client :: %v", e)
+	}
 
-	return nil, nil
+	log.Println(":: Instance client init ::")
+
+	return c
 }
 
-func NewFirewallsClient(ctx context.Context) (*compute.FirewallsClient, error) {
+func NewFirewallsClient(ctx context.Context, o option.ClientOption) *compute.FirewallsClient {
+	c, e := compute.NewFirewallsRESTClient(ctx, o)
+	if e != nil {
+		log.Fatalf("[INIT] Could not instantiate Firewall Client :: %v", e)
+	}
 
-	return nil, nil
+	log.Println(":: firewall client init ::")
+
+	return c
 }
 
-func NewMachinesTypeClient(ctx context.Context) (*compute.MachineTypesClient, error) {
+func NewMachinesTypeClient(ctx context.Context, o option.ClientOption) *compute.MachineTypesClient {
+	c, e := compute.NewMachineTypesRESTClient(ctx, o)
+	if e != nil {
+		log.Fatalf("[INIT] Could not instantiate Machine Type Client :: %v", e)
+	}
 
-	return nil, nil
+	log.Println(":: MT client init ::")
+
+	return c
 }
+func NewStorageClient(ctx context.Context, o option.ClientOption) *storage.Client {
+	c, e := storage.NewClient(ctx, o)
+	if e != nil {
+		log.Fatalf("[INIT] Could not instantiate Storage Client :: %v", e)
+	}
 
-func NewStorageClient(ctx context.Context) (*storage.Client, error) {
+	log.Println(":: storage client init ::")
 
-	return nil, nil
+	return c
 }
--- internal/service/validator.go
+++ internal/service/validator.go
@@ -2,11 +2,17 @@ package service
 
 import (
 	"context"
-	"log"
+	"path"
+	"strconv"
+	"time"
 
 	compute "cloud.google.com/go/compute/apiv1"
+	"cloud.google.com/go/compute/apiv1/computepb"
 	"cloud.google.com/go/storage"
+	"github.com/validator-gcp/v2/internal/apperror"
 	"github.com/validator-gcp/v2/internal/config"
+	"github.com/validator-gcp/v2/internal/models"
+	"google.golang.org/api/option"
 )
 
 type ValidatorService struct {
@@ -21,27 +27,13 @@ type ValidatorService struct {
 
 func NewValidatorService(cfg *config.Config) (*ValidatorService, error) {
 	ctx := context.Background()
+	o := option.WithCredentialsFile(cfg.GoogleCloud.ApplicationCredentials)
 
 	// All of this can be thought of @Autowired equivalent
-	fwClient, err := config.NewFirewallsClient(ctx)
-	if err != nil {
-		log.Fatalf("[INIT]: Could not start up Firewall Client")
-	}
-
-	instClient, err := config.NewInstancesClient(ctx)
-	if err != nil {
-		log.Fatalf("[INIT]: Could not start up Instances Client")
-	}
-
-	storageClient, err := config.NewStorageClient(ctx)
-	if err != nil {
-		log.Fatalf("[INIT]: Could not start up Storage Client")
-	}
-
-	mchTypeClient, err := config.NewMachinesTypeClient(ctx)
-	if err != nil {
-		log.Fatalf("[INIT]: Could not start up Machine Types Client")
-	}
+	fwClient := config.NewFirewallsClient(ctx, o)
+	instClient := config.NewInstancesClient(ctx, o)
+	storageClient := config.NewStorageClient(ctx, o)
+	mchTypeClient := config.NewMachinesTypeClient(ctx, o)
 
 	return &ValidatorService{
 		cfg:               cfg,
@@ -51,3 +43,79 @@ func NewValidatorService(cfg *config.Config) (*ValidatorService, error) {
 		machineTypeClient: mchTypeClient,
 	}, nil
 }
+
+/*
+Returns the detailed config of the VM running the minecraft server.
+*/
+func (s *ValidatorService) GetMachineDetails(ctx context.Context) (*models.InstanceDetailResponse, error) {
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second) // fair timeout?
+	defer cancel()
+
+	r := &computepb.GetInstanceRequest{
+		Project:  s.cfg.GoogleCloud.Project,
+		Instance: s.cfg.GoogleCloud.VMName,
+		Zone:     s.cfg.GoogleCloud.VMZone,
+	}
+
+	i, ie := s.instancesClient.Get(ctx, r)
+	if ie != nil {
+		return nil, apperror.MapError(ie)
+	}
+
+	var publicIp string
+	for _, nwInterface := range i.GetNetworkInterfaces() {
+
+		if len(nwInterface.GetAccessConfigs()) > 0 {
+			publicIp = nwInterface.GetAccessConfigs()[0].GetNatIP()
+			break
+		}
+	}
+
+	var mtName = path.Base(i.GetMachineType())
+
+	mtR := &computepb.GetMachineTypeRequest{
+		Project:     s.cfg.GoogleCloud.Project,
+		Zone:        s.cfg.GoogleCloud.VMZone,
+		MachineType: mtName,
+	}
+
+	mt, mte := s.machineTypeClient.Get(ctx, mtR)
+	if mte != nil {
+		return nil, apperror.MapError(mte)
+	}
+
+	var diskSize int64
+	if len(i.GetDisks()) > 0 {
+		diskSize = i.GetDisks()[0].GetDiskSizeGb()
+	}
+
+	// use the Getters (e.g., GetName()) instead of *i.Name for nil-safety.
+	res := &models.InstanceDetailResponse{
+
+		InstanceName:      i.GetName(),
+		InstanceZone:      path.Base(i.GetZone()),
+		MachineType:       mt.GetName(),
+		InstanceId:        strconv.FormatUint(i.GetId(), 10),
+		Status:            i.GetStatus(),
+		CreationTimestamp: i.GetCreationTimestamp(),
+		PublicIp:          publicIp,
+		CpuCores:          int(mt.GetGuestCpus()),
+		MemoryMb:          int(mt.GetMemoryMb()),
+		DiskGb:            int32(diskSize),
+	}
+
+	return res, nil
+}
+
+// TODO: implement these
+func (v *ValidatorService) DoPong()                             {}
+func (v *ValidatorService) AddIpToFirewall(request interface{}) {} // Use actual models later
+func (v *ValidatorService) IsIpPresent(ip string)               {}
+func (v *ValidatorService) PurgeFirewall()                      {}
+func (v *ValidatorService) AllowPublicAccess()                  {}
+func (v *ValidatorService) GetFirewallDetails()                 {}
+func (v *ValidatorService) GetServerInfo(address string)        {}
+func (v *ValidatorService) GetModList()                         {}
+func (v *ValidatorService) Download(object string)              {}
+func (v *ValidatorService) ExecuteRcon(address, command string) {}
